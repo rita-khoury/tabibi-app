5cdf4987b779d2ea17e287c1a357319bca597588
ramiarmoush <ramiarmoush11@gmail.com>
2026-08-20 14:04:15 +0300

    cleanup legacy patient queue

--- lib/features/navigation/binding/navigation_binding.dart
+++ lib/features/navigation/binding/navigation_binding.dart
@@ -1,12 +1,11 @@
-import 'package:tabibi/features/queue/controller/patient_queue_controller.dart';
 import 'package:get/get.dart';
 import 'package:tabibi/features/appointments/controller/appointments_controller.dart';
 import 'package:tabibi/features/favorites/controller/favorites_doctors_controller.dart';
 
 import '../../auth/repository/auth_repository.dart';
-import '../controller/navigation_controller.dart';
 import '../../profile/controller/profile_controller.dart';
 import '../../home/controller/home_controller.dart';
+import '../controller/navigation_controller.dart';
 
 class NavigationBinding extends Bindings {
   @override
@@ -16,6 +15,5 @@ class NavigationBinding extends Bindings {
     Get.lazyPut(() => ProfileController());
     Get.lazyPut(() => FavoritesDoctorsController(Get.find<AuthRepository>()));
     Get.lazyPut(() => AppointmentsController());
-    Get.lazyPut(() => PatientQueueController(Get.find<AuthRepository>()));
   }
 }
